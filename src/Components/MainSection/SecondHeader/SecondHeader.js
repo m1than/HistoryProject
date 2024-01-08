34ebd58c43972f76e5965eb0767c28701d7f02a5
Ivan Gaifutdinov <ig@foundersdao.io>
2024-01-08 17:37:45 +0300

    fix: attractions page

--- src/Components/MainSection/SecondHeader/SecondHeader.js
+++ src/Components/MainSection/SecondHeader/SecondHeader.js
@@ -1,49 +1,84 @@
 import { useTranslation } from "react-i18next"
+import { useDispatch } from 'react-redux';
+import { goTo } from '../../../feature/page.store';
 
 const SecondHeader = () => {
     const { i18n, t } = useTranslation();
+    const dispatch = useDispatch();
 
     return (
         <div className={'bg-[url("/public/header_backdround.png")]'}>
             <menu >
                 <ul className={'flex flex-row items-center justify-around g-x-5 h-[40px]'}>
-                    <li>
-                        <a href="#">{t('menu-depot')}</a>
+                    <li className={'cursor-pointer'}
+                        onClick={() => {
+                            dispatch(goTo('attractions'));
+                            const element = document.getElementById('depot');
+                            console.log(element);
+                            if (element) {
+                                element.scrollIntoView();
+                            }
+                        }}>
+                        <a>{t('menu-depot')}</a>
                     </li>
-                    <li>
-                        <a href="#">{t('menu-houses')}</a>
+                    <li className={'cursor-pointer'}
+                        onClick={() => {
+                            dispatch(goTo('attractions'));
+                            const element = document.getElementById('house-1');
+
+                            if (element) {
+                                element.scrollIntoView({ behavior: 'instant' });
+                            }
+                        }}>
+                        <a>{t('menu-houses')}</a>
                     </li>
-                    <li>
-                        <a href="#">{t('menu-church')}</a>
+                    <li className={'cursor-pointer'}
+                        onClick={() => {
+                            dispatch(goTo('attractions'));
+                            const element = document.getElementById('church');
+
+                            if (element) {
+                                element.scrollIntoView({ behavior: 'instant' });
+                            }
+                        }}>
+                        <a>{t('menu-church')}</a>
                     </li>
-                    <li>
-                        <a href="#">{t('menu-hospital')}</a>
+                    <li className={'cursor-pointer'}
+                        onClick={() => {
+                            dispatch(goTo('attractions'));
+                            const element = document.getElementById('hospital');
+
+                            if (element) {
+                                element.scrollIntoView({ behavior: 'instant' });
+                            }
+                        }}>
+                        <a>{t('menu-hospital')}</a>
                     </li>
-                    <li>
-                        <a href="#">{t('menu-railway')}</a>
+                    <li className={'cursor-pointer'}
+                        onClick={() => {
+                            dispatch(goTo('attractions'));
+                            const element = document.getElementById('vokzal');
+
+                            if (element) {
+                                element.scrollIntoView({ behavior: 'instant' });
+                            }
+                        }}>
+                        <a>{t('menu-railway')}</a>
                     </li>
-                    <li>
-                        <a href="#">{t('menu-water-tower')}</a>
+                    <li className={'cursor-pointer'}
+                        onClick={() => {
+                            dispatch(goTo('attractions'));
+                            const element = document.getElementById('water-tower');
+
+                            if (element) {
+                                element.scrollIntoView({ behavior: 'instant' });
+                            }
+                        }}>
+                        <a>{t('menu-water-tower')}</a>
                     </li>
                 </ul>
             </menu>
         </div>
-        // <header className={s.SecondHeader}>
-        //     <div className={s.header__background}>
-        //         <div className={s.header__container}>
-        //             <nav className={`${s.header__menu} ${s.menu}`}>
-        //                 <ul className={s.menu__list}>
-        //                     <li className={s.menu__item}><a href="" className={s.menu__link}>Депо</a></li>
-        //                     <li className={s.menu__item}><a href="" className={s.menu__link}>Дома</a></li>
-        //                     <li className={s.menu__item}><a href="" className={s.menu__link}>Церковь</a></li>
-        //                     <li className={s.menu__item}><a href="" className={s.menu__link}>Больница</a></li>
-        //                     <li className={s.menu__item}><a href="" className={s.menu__link}>Вокзал</a></li>
-        //                     <li className={s.menu__item}><a href="" className={s.menu__link}>Водонапорная башня</a></li>
-        //                 </ul>
-        //             </nav>
-        //         </div>
-        //     </div>
-        // </header>
     )
 }
 
